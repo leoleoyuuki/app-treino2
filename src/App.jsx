6bdbfbdb14468187a832d9022829397ef8ee7304
leoleoyuuki <rm550373@fiap.com.br>
2023-08-17 10:04:54 -0300

    Conexao git hub vercel para auto deploy

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>React App</h1>
+      <h2>Primeiro app vite + React</h2>
+      <p>Olá Mundo!!!</p>
+    </>
+  )
+}
+
+export default App
